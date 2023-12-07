dff16e02ecb449c9212a21f4017caa7e518088e5
dhanyasanth2002@gmail.com <dhanyasanth2002@gmail.com>
2023-12-07 23:43:42 +0530

    remove unwanted folders added

--- task-management-system/src/main/java/com/cn/taskmanagement/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cn.taskmanagement.repository;
-
-import com.cn.taskmanagement.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    // additional query methods if needed
-}
